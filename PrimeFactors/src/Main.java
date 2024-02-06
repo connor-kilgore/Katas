177c354c0e3163a63669100cf67234329f859a4e
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-05 17:09:47 -0700

    Added PrimeFactors Kata

---
+++ PrimeFactors/src/Main.java
@@ -0,0 +1,11 @@
+import primeFactors.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        PrimeFactorsTest test = new PrimeFactorsTest();
+        test.testOne();
+        test.testTwo();
+        test.testThree();
+        test.testFour();
+    }
+}
